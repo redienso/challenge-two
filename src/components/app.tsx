270bafb05a0c0766fe2d1656f4f9bf8f21c34e32
Osneider Martinez <redienso7@gmail.com>
2021-06-01 09:43:47 -0500

    get apps from back

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function App() {
+	React.useEffect(() => {
+		fetch("https://kuupanda.free.beeceptor.com/apps", { method: "get" })
+			.then((response) => response.json())
+			.then((json) => console.log(json));
+	}, []);
+
 	return <h1>Hello World</h1>;
 }
