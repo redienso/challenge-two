a593563bba7ad8ba9fb8450f26825e59be82c974
Osneider Martinez <redienso7@gmail.com>
2021-06-05 01:05:17 -0500

    add helvetica (regular & bold) fonts

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import jss from "jss";
 import preset from "jss-preset-default";
 import { SheetsRegistry } from "react-jss";
 import { JssProvider } from "react-jss";
+import HelveticaNeueBold from "./assets/fonts/helvetica-neue-bold.ttf";
+import HelveticaNeue from "./assets/fonts/helvetica-neue-regular.ttf";
 
 const setupJss = () => {
   jss.setup(preset());
@@ -14,9 +16,20 @@ const setupJss = () => {
   const globalStyleSheet = jss
     .createStyleSheet({
       "@global": {
+        "@font-face": [
+          {
+            fontFamily: "HelveticaNeueBold",
+            src: `url(${HelveticaNeueBold})`,
+          },
+          {
+            fontFamily: "HelveticaNeue",
+            src: `url(${HelveticaNeue})`,
+          },
+        ],
         body: {
           margin: 0,
           background: "#f5f5f5",
+          fontFamily: "HelveticaNeue",
         },
       },
     })
--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,45 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "development",
-	entry: "./src/index.tsx",
-	output: {
-		filename: "index.js",
-		path: path.resolve(__dirname, "dist"),
-	},
-	devServer: {
-		contentBase: "./dist",
-	},
-	devtool: "source-map",
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Apdex Board",
-			template: "./src/index.html",
-		}),
-	],
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	module: {
-		rules: [
-			{
-				test: /.(js|tsx?)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [
-							"@babel/preset-env",
-							"@babel/preset-react",
-							"@babel/preset-typescript",
-						],
-					}
-				},
-			}
-		]
-	},
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Apdex Board",
+      template: "./src/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|tsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.ttf$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
